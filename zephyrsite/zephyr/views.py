4850856e6f2ff02a9dd0c5998fa115a20732b471
MayankPanda <mayankpanda7305@gmail.com>
2022-11-04 09:18:14 +0530

    added static and templates

--- zephyrsite/zephyr/views.py
+++ zephyrsite/zephyr/views.py
@@ -3,5 +3,9 @@ from django.http import HttpResponse
 
 
 def index(request):
+    #return render(request, 'homepage.html')
     return HttpResponse("Hello, world. You're at the polls index.")
 # Create your views here.
+
+def home(request):
+    return render(request, 'homepage.html')
