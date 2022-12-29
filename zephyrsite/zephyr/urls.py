41c5299d0f81cc96d9f6f6221cfe1eeacd5d9aed
MayankPanda <mayankpanda7305@gmail.com>
2022-12-29 18:53:58 +0530

    added hyperlinks

--- zephyrsite/zephyr/urls.py
+++ zephyrsite/zephyr/urls.py
@@ -4,14 +4,14 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('home', views.home_view,),
-    path('message', views.message_test,),
-    path('testh', views.home_test,),
-    path('testinc', views.inc_test,),
-    path('testresp', views.resp_test,),
-    path('testlayout', views.layout_test,),
+    path('testlayout', views.home_view,),#Nothing
+    path('register', views.message_test,),#reg form
+    path('testh', views.home_test,),#Nothing
+    path('incentives', views.inc_test,),#incentives
+    path('responsibilities', views.resp_test,),#responsibility
+    path('home', views.layout_test,),#home
     path('testphonehome', views.phone_home_test,),
-    path('testhamburger',views.hamburger),
-    path('success',views.reg_success),
-    path('about',views.about),
+    path('testhamburger',views.hamburger),#Phone incentives page
+    path('success',views.reg_success),#success page
+    path('about',views.about),#about page
 ]
